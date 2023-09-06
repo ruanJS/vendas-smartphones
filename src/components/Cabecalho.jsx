76a43da7cc417c93a6d9701f9dd514057829d68d
ruanJS <rm551096@fiap.com.br>
2023-09-06 17:55:43 -0300

    arrumando lista e cabecalho

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -5,7 +5,7 @@ export default function Cabecalho(props){
     return(
         <>
             <header>
-                <h1>Venda de Aparelhos</h1>
+                <h1>Venda de aparelhos</h1>
                 {/*Recuperar prop.children de Lista */}
                 
                 <ul>
